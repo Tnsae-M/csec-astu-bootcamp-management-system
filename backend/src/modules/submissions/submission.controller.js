a69c14af1eefc075cb0a7e4b65ecf7e3db586a58
Wogari-GH6877 <wogariararsa0968@gmail.com>
2026-04-20 23:00:17 -0700

    submission feature is done

---
+++ backend/src/modules/submissions/submission.controller.js
@@ -0,0 +1,70 @@
+import * as submissionService from "./submission.service.js";
+
+// POST /submissions
+export const submitTask = async (req, res, next) => {
+  try {
+    const submission = await submissionService.submitTask(
+      req.body,
+      req.user.userId
+    );
+
+    res.status(201).json({
+      success: true,
+      message: "Task submitted",
+      data: submission,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+// GET /submissions/me
+export const getMySubmissions = async (req, res, next) => {
+  try {
+    const data = await submissionService.getMySubmissions(
+      req.user.userId
+    );
+
+    res.status(200).json({
+      success: true,
+      data,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+// GET /submissions/task/:taskId
+export const getSubmissionsByTask = async (req, res, next) => {
+  try {
+    const data = await submissionService.getSubmissionsByTask(
+      req.params.taskId
+    );
+
+    res.status(200).json({
+      success: true,
+      data,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+// PUT /submissions/:id/grade
+export const gradeSubmission = async (req, res, next) => {
+  try {
+    const data = await submissionService.gradeSubmission(
+      req.params.id,
+      req.body,
+      req.user.userId
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Submission graded",
+      data,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
