d016a95d509d685c5b0a2ccfacf85b607e228ddf
Wogari-GH6877 <wogariararsa0968@gmail.com>
2026-04-20 22:53:32 -0700

    enrollments feauture is done

---
+++ backend/src/modules/enrollments/enrollment.controller.js
@@ -0,0 +1,73 @@
+import * as enrollmentService from "./enrollment.service.js";
+
+// POST /enrollments
+export const enroll = async (req, res, next) => {
+  try {
+    const { userId, bootcampId } = req.body;
+
+    const enrollment = await enrollmentService.enrollUser(
+      userId,
+      bootcampId
+    );
+
+    res.status(201).json({
+      success: true,
+      message: "User enrolled successfully",
+      data: enrollment,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+// GET /enrollments/bootcamp/:id
+export const getBootcampEnrollments = async (req, res, next) => {
+  try {
+    const data = await enrollmentService.getBootcampEnrollments(
+      req.params.id
+    );
+
+    res.status(200).json({
+      success: true,
+      data,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+// GET /enrollments/me
+export const getMyEnrollments = async (req, res, next) => {
+  try {
+    const data = await enrollmentService.getMyEnrollments(
+      req.user.userId
+    );
+
+    res.status(200).json({
+      success: true,
+      data,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+// PUT /enrollments/:id
+export const updateStatus = async (req, res, next) => {
+  try {
+    const { status } = req.body;
+
+    const updated = await enrollmentService.updateEnrollmentStatus(
+      req.params.id,
+      status
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Enrollment updated",
+      data: updated,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
---
+++ backend/src/modules/enrollments/enrollment.service.js
@@ -0,0 +1,54 @@
+import Enrollment from "./enrollment.model.js";
+import User from "../users/user.model.js";
+import Bootcamp from "../bootcamps/bootcamp.model.js";
+
+const buildError = (message, statusCode = 400) => {
+  const err = new Error(message);
+  err.statusCode = statusCode;
+  return err;
+};
+
+// Enroll user
+export const enrollUser = async (userId, bootcampId) => {
+  const user = await User.findById(userId);
+  if (!user) throw buildError("User not found", 404);
+
+  if (user.role !== "student") {
+  throw buildError("Only students can be enrolled", 400);
+}
+
+  const bootcamp = await Bootcamp.findById(bootcampId);
+  if (!bootcamp) throw buildError("Bootcamp not found", 404);
+
+  const existing = await Enrollment.findOne({ userId, bootcampId });
+  if (existing) {
+    throw buildError("User already enrolled in this bootcamp", 409);
+  }
+
+  return await Enrollment.create({ userId, bootcampId });
+};
+
+// Get students in a bootcamp
+export const getBootcampEnrollments = async (bootcampId) => {
+  return await Enrollment.find({ bootcampId })
+    .populate("userId", "name email role status")
+    .populate("bootcampId", "name");
+};
+
+//  Get current user's enrollments
+export const getMyEnrollments = async (userId) => {
+  return await Enrollment.find({ userId })
+    .populate("bootcampId", "name startDate endDate status");
+};
+
+//  Update enrollment status
+export const updateEnrollmentStatus = async (id, status) => {
+  const enrollment = await Enrollment.findById(id);
+
+  if (!enrollment) throw buildError("Enrollment not found", 404);
+
+  enrollment.status = status;
+  await enrollment.save();
+
+  return enrollment;
+};
