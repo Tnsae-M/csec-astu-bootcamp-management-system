d1dda99a9a36aaef4eb0f42cae255398b9ec2150
Tnsae <tinsaemk6679@gmail.com>
2026-04-23 18:32:24 +0300

    chore: just refined somethings

--- backend/src/modules/divisions/division.route.js
+++ backend/src/modules/divisions/division.route.js
@@ -10,6 +10,7 @@ import { authGuard, roleGuard } from "../../middleware/role.guard.js";
 const router = express.Router();
 
 router.post("/", roleGuard("admin"), divisionController.createDivision);
+//get divisions can be accessed by every role since their will be a division icon on the sidebar for every user to click and view the bootcamps under that division.
 router.get("/", authGuard, divisionController.getDivisions);
 router.get("/:id", authGuard, divisionController.getDivisionById);
 router.put(
